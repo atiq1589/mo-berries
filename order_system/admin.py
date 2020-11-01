a4553b01a6fb4dd239a3a5af52f77081abd8ce3a
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2020-11-01 17:56:23 +0600

    add pizza in admin

--- order_system/admin.py
+++ order_system/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from order_system.base import BaseModelAdmin
+from order_system.models import Pizza
 # Register your models here.
+
+
+@admin.register(Pizza)
+class PizzaAdmin(BaseModelAdmin):
+  list_display = ('name', 'flavor', 'size', 'created_by', 'created_at', 'modified_by', 'modified_at')
