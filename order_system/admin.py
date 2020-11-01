8fe5f5ab376e0eb7b5664f28f43cd58c3ce80a4e
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2020-11-01 18:37:06 +0600

    register order and order line in admin

--- order_system/admin.py
+++ order_system/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
-from order_system.base import BaseModelAdmin
-from order_system.models import Pizza
+from order_system.base import BaseModelAdmin, BaseModelTabularInlineAdmin
+from order_system.models import Pizza, Order, OrderLine
 # Register your models here.
 
+
+admin.site.empty_value_display = '(empty)'
 
 @admin.register(Pizza)
 class PizzaAdmin(BaseModelAdmin):
   list_display = ('name', 'flavor', 'size', 'created_by', 'created_at', 'modified_by', 'modified_at')
+
+
+class OrderLineInline(BaseModelTabularInlineAdmin):
+  model = OrderLine
+
+
+@admin.register(Order)
+class OrderAdmin(BaseModelAdmin):
+  list_display = ('customer_name', 'created_by', 'created_at', 'modified_by', 'modified_at')
+  inlines = [
+    OrderLineInline
+  ]
+  
