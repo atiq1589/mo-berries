842c4cc023a141c255cd3389ff96217c5c1c8fac
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2020-11-06 17:57:37 +0600

    add test cases for order line add check in already delivered order

--- order_system/tests/order_line.py
+++ order_system/tests/order_line.py
@@ -2,13 +2,15 @@ from django.test import TestCase
 from order_system.models import OrderLine, Order
 from rest_framework.test import APIClient
 from rest_framework import status
-from order_system.enums import PizzaSizeEnum, PizzaFlavorEnum
+from order_system.enums import PizzaSizeEnum, PizzaFlavorEnum, OrderStatusEnum
 
 
 class OrderLineTestCase(TestCase):
   def setUp(self):
     self.client = APIClient(enforce_csrf_checks=False)
     self.order = Order.objects.create(customer_name="Md. Atiqul Islam", customer_address="Dhaka, Bangladesh")
+    self.delivered_order = Order.objects.create(customer_name="Md. Atiqul Islam", customer_address="Dhaka, Bangladesh", \
+      order_status=OrderStatusEnum.DELIVERED.value)
 
   def test_order_line_create(self):
     """
@@ -37,3 +39,11 @@ class OrderLineTestCase(TestCase):
     self.assertEqual(response.data[0].get('flavor'), PizzaFlavorEnum.MARINARA.value)
     self.assertEqual(response.data[0].get('size'), PizzaSizeEnum.LARGE.value)
     self.assertEqual(response.data[0].get('quantity'), 1)
+
+  def test_add_order_line_in_delivered_order(self):
+    payload = dict(flavor=PizzaFlavorEnum.MARINARA.value, size=PizzaSizeEnum.LARGE.value, quantity=1, order=self.delivered_order.id)
+
+    response = self.client.post('/api/v1/order-lines/', payload, format='json')
+
+    self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    self.assertEqual(OrderLine.objects.count(), 0)
