5ff3f418c297f2b1a92bc4a5d4245824fa57d1f9
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2020-11-06 17:43:07 +0600

    add order update validation check for delivered order

--- order_system/seralizers/order.py
+++ order_system/seralizers/order.py
@@ -1,6 +1,8 @@
 from order_system.base.serializers import BaseModelSerializer
 from order_system.models import Order, OrderLine
 from rest_framework import serializers
+from order_system.enums import OrderStatusEnum
+from gettext import gettext as _
 
 
 class OrderLineSerializer(BaseModelSerializer):
@@ -17,6 +19,13 @@ class OrderSerializer(BaseModelSerializer):
   Order Serializer
   """
   order_line = serializers.PrimaryKeyRelatedField(many=True, queryset=OrderLine.objects.all(), required=False)
+
+  def update(self, instance, validated_data):
+    if instance.order_status == OrderStatusEnum.DELIVERED.value:
+      raise serializers.ValidationError(_("Order Already Delivered. You can't change already delivered order."))
+
+    return super().update(instance, validated_data)
+    
   class Meta:
     model = Order
     exclude = ('created_by', 'modified_by', 'created_at', 'modified_at')
